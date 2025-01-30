15015f1a092350477e1be671a3a46c695fc1345e
java_ada <leonardoptluz@hotmail.com>
2025-01-30 17:38:30 -0300

    Initial commit

---
+++ src/test/java/com/example/marketproject/market/MarketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marketproject.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
