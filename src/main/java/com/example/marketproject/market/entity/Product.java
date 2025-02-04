a60f41eff65804a4b7b400f80a26365429b0653f
java_ada <leonardoptluz@hotmail.com>
2025-02-03 21:51:56 -0300

    Product -> update

--- src/main/java/com/example/marketproject/market/entity/Product.java
+++ src/main/java/com/example/marketproject/market/entity/Product.java
@@ -1,5 +1,9 @@
 package com.example.marketproject.market.entity;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,7 +14,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "products")
+@Table(name = "tb_products")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -18,10 +22,24 @@ public class Product {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
     private Long id;
 
+    @Column(name = "product_name", nullable = false, length = 255)
     private String name;
+
+    @Column(name = "product_description", nullable = false, columnDefinition = "TEXT")
     private String description;
-    private Double price;
+
+    @Column(name = "product_price", nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+
+    @Column(name = "product_stock", nullable = false)
     private Integer stock;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "uptadet_at")
+    private LocalDateTime updateAt;
 }
