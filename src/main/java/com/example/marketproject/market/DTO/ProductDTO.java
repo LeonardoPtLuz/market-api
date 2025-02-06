33b6d3d410e8d09878505bbafd1f1ad9bdd3c14e
java_ada <leonardoptluz@hotmail.com>
2025-02-06 17:35:59 -0300

    Conflito tipo de variável -> Resolvido

--- src/main/java/com/example/marketproject/market/DTO/ProductDTO.java
+++ src/main/java/com/example/marketproject/market/DTO/ProductDTO.java
@@ -1,5 +1,7 @@
 package com.example.marketproject.market.DTO;
 
+import java.math.BigDecimal;
+
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -22,7 +24,7 @@ public class ProductDTO {
 
     @NotNull(message = "É necessário informar o preço!")
     @Min(value = 0, message = "O valor preço deve ser <= 0")
-    private Double price;
+    private BigDecimal price;
 
     @NotNull(message = "É necessário informar quantidade do produto em estoque!")
     @Min(value = 0, message = "O estoque deve ser <= 0")
